fb8384912d049a8105a79d892f1b42bc9af7ed8e
pwmhm <pwillyam@gmail.com>
2021-04-15 14:27:10 +0200

    Test 1.1
    -Removed testing script. (Merged to main file)
    -Added prediction script and plotting script to analyze model manually.

--- test.py
+++ test.py
@@ -1,18 +1,43 @@
 import tensorflow as tf
+from numpy import load
+import csv
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
+from random import randint
 
-# learning_rate = 1e-5   #standard learning rate2
-# epochs        = 11     #massive data
-# batch_size    = 64
-# dropout_rate  = 0.5
+image = load("Dataset/test/dogs_vs_cats_images_test.npy")
+label = load("Dataset/test/dogs_vs_cats_labels_test.npy")
+model_name = "The_Model.h5"
 
-test_dataset = tf.data.experimental.load("processed_dataset/test/", (tf.TensorSpec(shape=(300, 300, 3), dtype=tf.float32, name=None),
- tf.TensorSpec(shape=(), dtype=tf.int64, name=None)), compression="GZIP")
+print(len(label))
 
-test_dataset = test_dataset.batch(64)
 
-model = tf.keras.models.load_model("The_Model.h5")
+model = tf.keras.models.load_model(model_name)
 
-results = model.evaluate(test_dataset)
+model.summary()
+
+results = model.predict(image)
+
+plt.figure(figsize=(10, 10))
+for i in range(48) :
+    j = randint(0,len(image))
+    labs = "doge"
+    if float(results[j]) < 0.5 :
+        labs = "catto"
+    plt.subplot(7, 7, i+1)
+    plt.imshow(image[j]/255.0)
+    plt.title(labs)
+plt.show()
+
+
+####for checking log loss in kaggle
+str_csv = "submission_" + model_name + ".csv"
+
+with open(str_csv, 'w', newline='') as file:
+    fieldnames = ['id', 'label']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for i in range(len(results)) :
+        writer.writerow({'id': label[i], 'label': str(results[i])[1:-1]})
